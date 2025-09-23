c96bc9af021a6c38e7f1ce7ee186429061707d71
vladimir.y.bogomolov@gmail.com <vladimir.y.bogomolov@gmail.com>
2025-09-23 15:34:31 +0200

    Finished finding and positioning

---
+++ scan.py
@@ -0,0 +1,61 @@
+import py_trees
+from robot_helpers import find_object, robot_camera_position_encoders
+
+class FindObject(py_trees.behaviour.Behaviour):
+    def __init__(self, name, blackboard):
+        super(FindObject, self).__init__(name)
+        self.robot = blackboard.read('robot')
+    def setup(self):
+        self.timestep = int(self.robot.getBasicTimeStep())
+        self.camera = self.robot.getDevice("camera")
+        self.camera.enable(self.timestep)
+        self.left_motor = self.robot.getDevice('wheel_left_joint')
+        self.left_motor.setPosition(float('inf'))
+        self.right_motor = self.robot.getDevice('wheel_right_joint')
+        self.right_motor.setPosition(float('inf'))
+        for _, sensor_name in robot_camera_position_encoders.items():
+                sensor = self.robot.getDevice(sensor_name)
+                if sensor:
+                    sensor.enable(self.timestep)
+    def update(self):
+        self.left_motor.setVelocity(0)
+        self.right_motor.setVelocity(0)
+        
+        (found, coordinates) = find_object(self.camera, self.timestep, self.robot)
+        if found:
+            delta = 0.015
+
+            # Align y axis
+            if (coordinates[1] < -delta or coordinates[1] > delta):
+                print("Aligning Y axis")
+                # Ensure enough space
+                if (coordinates[0] < 1.45):
+                    self.left_motor.setVelocity(-0.5)
+                    self.right_motor.setVelocity(-0.5)
+                    return py_trees.common.Status.RUNNING
+                if (coordinates[1] < 0):
+                    # Turn right
+                    self.right_motor.setVelocity(-0.5)
+                    self.left_motor.setVelocity(0.5)
+                else:
+                    # Turn left
+                    self.left_motor.setVelocity(-0.5)
+                    self.right_motor.setVelocity(0.5)
+                return py_trees.common.Status.RUNNING
+            else:
+                print("Alining X axis")
+                arm_length = 1.43
+                if (coordinates[0] > arm_length):
+                    self.left_motor.setVelocity(0.5)
+                    self.right_motor.setVelocity(0.5)
+                    return py_trees.common.Status.RUNNING
+            return py_trees.common.Status.SUCCESS
+        else:
+            self.left_motor.setVelocity(0.5)
+            self.right_motor.setVelocity(-0.5)
+            return py_trees.common.Status.RUNNING
+    
+    def terminate(self, new_status):
+        print("Ready to grab the object")
+        self.left_motor.setVelocity(0)
+        self.right_motor.setVelocity(0)
--- tiago_arm.py
+++ tiago_arm.py
@@ -7,45 +7,9 @@ from py_trees.composites import Sequence, Parallel, Selector
 from mapping import Mapping
 from navigation import Navigation
 from planning import Planning
+from scan import FindObject
 from set_position import SetPosition
-from camera_helpers import camera_to_base
-
-positions = {
-    'safe': {
-       'torso_lift_joint': 0.35,
-       'arm_1_joint': 0.71,
-       'arm_2_joint': 1.02,
-       'arm_3_joint': -2.815,
-       'arm_4_joint': 1.011,
-       'arm_5_joint': 0,
-       'arm_6_joint': 0,
-       'arm_7_joint': 0,
-       'gripper_left_finger_joint': 0,
-       'gripper_right_finger_joint': 0,
-       'head_1_joint': 0,
-       'head_2_joint': 0
-    },
-    'open': {
-       'torso_lift_joint': 0.35,
-       'arm_1_joint': 1.5,
-       'arm_2_joint': 0,
-       'arm_3_joint': 0,
-       'arm_4_joint': 0,
-       'arm_5_joint': 0,
-       'arm_6_joint': 0,
-       'arm_7_joint': -1.5,
-       'gripper_left_finger_joint': 0.04,
-       'gripper_right_finger_joint': 0.04,
-       'head_1_joint': 0,
-       'head_2_joint': 0
-    }
-}
-
-robot_encoders = {
-       'torso_lift_joint': 'torso_lift_joint_sensor',
-       'head_1_joint': 'head_1_joint_sensor',
-       'head_2_joint': 'head_2_joint_sensor'
-    }
+from robot_helpers import positions
 
 # create the Robot instance.
 robot = Supervisor()
@@ -64,55 +28,11 @@ class DoesMapExist(py_trees.behaviour.Behaviour):
         else:
             print("No map found. Start mapping")
             return py_trees.common.Status.FAILURE
-        
-class FindObject(py_trees.behaviour.Behaviour):
-    def __init__(self, name, blackboard):
-        super(FindObject, self).__init__(name)
-        self.robot = blackboard.read('robot')
-        self.blackboard = blackboard
-    def setup(self):
-        # enable sensors
-        self.timestep = int(self.robot.getBasicTimeStep())
-        self.camera = self.robot.getDevice("camera")
-        self.camera.enable(self.timestep)
-        self.left_motor = self.robot.getDevice('wheel_left_joint')
-        self.left_motor.setPosition(float('inf'))
-        self.right_motor = self.robot.getDevice('wheel_right_joint')
-        self.right_motor.setPosition(float('inf'))
-        for _, sensor_name in robot_encoders.items():
-            sensor = self.robot.getDevice(sensor_name)
-            if sensor:
-                sensor.enable(self.timestep)
-    def update(self):
-        self.left_motor.setVelocity(0.5)
-        self.right_motor.setVelocity(-0.5)
-        
-        self.camera.recognitionEnable(self.timestep)
-        self.objects = self.camera.getRecognitionObjects()
-        if len(self.objects) > 0:
-            self.left_motor.setVelocity(0)
-            self.right_motor.setVelocity(0)
-            for object in self.objects:
-                print(list(object.getPosition()))
-            current_object = self.objects[0]
-            # TO DO: recalculate to base robot coordinates
-            current_object_position = list(current_object.getPosition())
-            self.blackboard.write('objects', current_object_position)
-            sensor_values = {}
-            for _, sensor_name in robot_encoders.items():
-                sensor = self.robot.getDevice(sensor_name)
-                if sensor:
-                    current_value = sensor.getValue()
-                    sensor_values[sensor_name] = current_value
-            print(camera_to_base(current_object_position[0], current_object_position[1], current_object_position[2], sensor_values['torso_lift_joint_sensor'], sensor_values['head_1_joint_sensor'], sensor_values['head_2_joint_sensor']))
-            return py_trees.common.Status.SUCCESS
-        return py_trees.common.Status.RUNNING
 
 class Stop(py_trees.behaviour.Behaviour):
     def __init__(self, name, blackboard):
         super(Stop, self).__init__(name)
         self.robot = blackboard.read('robot')
-        self.blackboard = blackboard
     def setup(self):
         # enable sensors
         self.timestep = int(self.robot.getBasicTimeStep())
@@ -123,7 +43,7 @@ class Stop(py_trees.behaviour.Behaviour):
     def update(self):
         self.left_motor.setVelocity(0)
         self.right_motor.setVelocity(0)
-        return py_trees.common.Status.RUNNING
+        return py_trees.common.Status.RUNNING  
              
 class Blackboard:
     def __init__(self):
@@ -148,8 +68,9 @@ tree = Sequence("Main", children=[
             ], memory=True),
             Planning("Compute path to the counter", blackboard, (0.71, 0.41)),
             Navigation("Move to the counter", blackboard),
-            FindObject("Find target object to grasp", blackboard),
             SetPosition("Put arm in target position", blackboard, positions['open']),
+            FindObject("Find target object to grasp", blackboard),
+            # PickTheObject("Pick the object up", blackboard),
             Stop("Stop", blackboard)
        ], memory=True)
        
